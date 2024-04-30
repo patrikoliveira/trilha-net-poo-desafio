6190221a272ca80a0390f8bcd16e9b227c71adf4
Patrik Oliveira <patrikro.oliveira@gmail.com>
2024-04-30 11:27:45 -0300

    chore: cria um sistema de celular - desafio dio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,13 +3,19 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string modelo;
+        private string IMEI;
+        private int memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            this.modelo = modelo;
+            IMEI = imei;
+            this.memoria = memoria;
         }
+
+        public string Modelo => modelo;
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphone IPhone");
+Smartphone iphone = new Iphone("123", "IPhone 14", "123456789", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Iphone("321", "Nokia ABC1", "987654321", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
